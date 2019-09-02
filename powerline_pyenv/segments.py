431d43f4e6ef7382189c1aa064a509a095514379
Sinkerine <github@15cm.net>
2019-09-02 00:20:17 -0700

    Fix subprocess pyenv version locked by python that runs powerline
    Remove env vars injected by pyenv

--- powerline_pyenv/segments.py
+++ powerline_pyenv/segments.py
@@ -1,10 +1,22 @@
+import os
 import subprocess
 
 from powerline.theme import requires_segment_info
 
+env_pyenv_keys = ["PYENV_VERSION", "PYENV_DIR"]
+
+# Remove ENV vars injected to powershell by pyenv
+for key in env_pyenv_keys:
+    os.unsetenv(key)
+
 
 @requires_segment_info
 def pyenv(pl, segment_info):
+    global env_pyenv_keys
+    env = segment_info["environ"]
+    for key in env_pyenv_keys:
+        if key in env:
+            os.putenv(key, env[key])
     cwd = segment_info["getcwd"]()
     pyenv_version = subprocess.check_output(
         ["pyenv", "version-name"], cwd=cwd, encoding="utf8"
